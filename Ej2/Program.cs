dc837420f9f40edf58a3d3589437af1068446f1c
Mxnuu7_ <manu17pc@gmail.com>
2022-10-15 17:28:12 +0200

    Fin

--- Ej2/Program.cs
+++ Ej2/Program.cs
@@ -4,13 +4,35 @@ using Ej2;
 
 Electrodomestico[] electrodomesticos = new Electrodomestico[10];
 
-electrodomesticos[0] = new Electrodomestico();
+electrodomesticos[0] = new Lavadora();
 electrodomesticos[1] = new Lavadora();
 electrodomesticos[2] = new Television();
-electrodomesticos[3] = new Electrodomestico(50, 15);
+electrodomesticos[3] = new Television(50, 15);
 electrodomesticos[4] = new Lavadora(300, 200);
 electrodomesticos[5] = new Television(600, 50);
-electrodomesticos[6] = new Electrodomestico(399.99, Colores.AZUL, 'F', 0.2);
-electrodomesticos[7] = new Electrodomestico();
-electrodomesticos[8] = new Electrodomestico();
-electrodomesticos[9] = new Electrodomestico();
+electrodomesticos[6] = new Lavadora(5, 399.99, Colores.AZUL, 'F', 0.2);
+electrodomesticos[7] = new Lavadora(25, 399.99, Colores.AZUL, 'F', 0.2);
+electrodomesticos[8] = new Television(27, true, 399.99, Colores.AZUL, 'F', 0.2);
+electrodomesticos[9] = new Television();
+
+Double sumaLavadora = 0, sumaTelevision = 0, sumaElectrodomesticos = 0;
+
+foreach (var electrodomestico in electrodomesticos)
+{
+	electrodomestico.PrecioFinal();
+
+	if (electrodomestico is Lavadora)
+	{
+		sumaLavadora += electrodomestico.PrecioBase;
+	}
+	else
+	{
+		sumaTelevision += electrodomestico.PrecioBase;
+	}
+
+	sumaElectrodomesticos += electrodomestico.PrecioBase;
+}
+
+Console.WriteLine($"Suma de todas las televisiones: {sumaTelevision}€");
+Console.WriteLine($"Suma de todas las lavadoras: {sumaLavadora}€");
+Console.WriteLine($"Suma de todos los electrodomésticos: {sumaElectrodomesticos}€");
