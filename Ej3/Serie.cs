59d083aed788c3a7fe6edb5dda05c854316780a3
MJ-Curtido <manu17pc@gmail.com>
2022-10-17 10:23:14 +0200

    V1-Falta medio del 3

---
+++ Ej3/Serie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3
+{
+    internal class Serie
+    {
+        public String Titulo { get; set; } = "El surfista programador";
+        public Int16 NumTemp { get; set; } = 3;
+        public Boolean Entregado { get; set; } = false;
+        public String Genero { get; set; } = "Suspense";
+        public String Creador { get; set; } = "Miguelito";
+
+        public Serie()
+        {
+
+        }
+
+        public Serie(String Titulo, String Creador)
+        {
+            this.Titulo = Titulo;
+            this.Creador = Creador;
+        }
+
+        public Serie(string Titulo, short NumTemp, string Genero, string Creador)
+        {
+            this.Titulo = Titulo;
+            this.NumTemp = NumTemp;
+            this.Genero = Genero;
+            this.Creador = Creador;
+        }
+
+        public override string ToString()
+        {
+            return $"Titulo: {this.Titulo}, numero de temporadas: {this.NumTemp}, entregado: {this.Entregado}, genero: {this.Genero}, creador: {this.Creador}.";
+        }
+    }
+}
---
+++ Ej3/Videojuego.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3
+{
+    internal class Videojuego
+    {
+        public String Titulo { get; set; } = "Macarrones con tomate";
+        public Int16 HorasEstimadas { get; set; } = 10;
+        public Boolean Entregado { get; set; } = false;
+        public String Genero { get; set; } = "Terror";
+        public String Companyia { get; set; } = "Manuelete";
+
+        public Videojuego()
+        {
+
+        }
+
+        public Videojuego(String Titulo, String Creador)
+        {
+            this.Titulo = Titulo;
+            this.Companyia = Creador;
+        }
+
+        public Videojuego(string Titulo, short HorasEstimadas, string Genero, string Creador)
+        {
+            this.Titulo = Titulo;
+            this.HorasEstimadas = HorasEstimadas;
+            this.Genero = Genero;
+            this.Companyia = Creador;
+        }
+
+        public override string ToString()
+        {
+            return $"Titulo: {this.Titulo}, horas estimadas: {this.HorasEstimadas}, entregado: {this.Entregado}, genero: {this.Genero}, compañía: {this.Companyia}.";
+        }
+    }
+}
